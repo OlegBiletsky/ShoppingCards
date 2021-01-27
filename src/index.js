9f0ebdc55b5bad1bdde8a44d0a78c541685713ea
OlegBiletsky <ol.biletsky@gmail.com>
2021-01-27 09:19:46 +0200

    create index.js in src

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import {BrowserRouter as Router, } from 'react-router-dom';
 import './index.css';
 import App from './containers/App';
+import rootReducer from './reducers';
+
+
+// // LOGGER
+// const middleware = [ thunk ];
+// if (process.env.NODE_ENV !== 'production') {
+//     middleware.push(createLogger());
+// }
+
+// // REDUX-DEV-TOOLS
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// //CREATE STORE
+// const store = createStore(rootReducer, composeEnhancers(
+//     applyMiddleware(...middleware)
+// ));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+        // <Provider store={store}>
+            <Router>
+                <App />
+            </Router>,
+        // </Provider>,
+    document.getElementById('root')
+  );
