c43e116c4cb13c1cc8d28811321091d961e49f79
OlegBiletsky <ol.biletsky@gmail.com>
2021-02-02 10:51:36 +0200

    refac structure

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import Card from '../Card';
-import BuyCheapest from '../BuyCheapest';
+import Card from './Card';
+import BuyCheapest from './BuyCheapest';
 
 
 const AppField = styled.div`
